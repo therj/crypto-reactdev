bbf04d67a6cd6a8c2a807aa6d70a984a5fdb037c
Rabindra Joshi <therj@users.noreply.github.com>
2018-06-10 23:39:25 +0545

    added starter files

---
+++ starter-files/src/App.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react'
+import './App.css'
+import Crypto from './components/Crypto'
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <div className="App-header">
+          <h2> Crypto ReactDev </h2>{' '}
+        </div>
+      </div>
+    )
+  }
+}
+
+export default App
